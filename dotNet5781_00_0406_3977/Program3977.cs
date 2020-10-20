41e3a425e1feca68796e302ee32c0da004b3ca2b
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2020-10-20 23:06:51 +0300

    Initial code + Welcome3977

---
+++ dotNet5781_00_0406_3977/Program3977.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_0406_3977
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3977();
+            Welcome0406();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome0406();
+
+        private static void Welcome3977()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+
+    }
+}
