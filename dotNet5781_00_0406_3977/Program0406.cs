1aaf778b0ea4625dad92689b64ad0ad2de4f66f2
אודיה סעדון <osadoun@g.jct.ac.il>
2020-10-21 10:01:26 +0300

    add function

---
+++ dotNet5781_00_0406_3977/Program0406.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_0406_3977
+{
+    partial class Program
+    {
+        static partial void Welcome0406()
+        {
+            Console.WriteLine("We are winners!!!!");
+        }
+    }
+}
