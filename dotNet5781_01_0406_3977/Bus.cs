273c21610966982561dfcecb0cbe1357b0037cab
אודיה סעדון <osadoun@g.jct.ac.il>
2020-10-28 20:02:30 +0200

    class Bus

--- dotNet5781_01_0406_3977/Bus.cs
+++ dotNet5781_01_0406_3977/Bus.cs
@@ -1,7 +1,9 @@
 ﻿
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
     using System.Runtime.Remoting.Metadata.W3cXsd2001;
     using System.Text;
     using System.Threading.Tasks;
@@ -10,26 +12,13 @@
     {
         struct Date
         {
-            int day;
-            int month;
-            int year;
+            public int day;
+            public int month;
+            public int year;
         };
-
-        class Bus1
+        class Bus
         {
-            private int year;
 
-            public int Year
-            {
-                get { return year; }
-                set
-                {
-                    if (year < 0) //year can't be a negative number
-                        year = 0;
-                    else
-                        year = value;
-                }
-            }
 
             private string licenseNumber;
 
@@ -39,7 +28,7 @@
                 set
                 {
                     string temp = value;
-                    if ((year < 2018) && (temp.Length == 7))
+                    if (temp.Length == 7)
                     {
                         for (int i = 0; i < temp.Length; i++)
                         {
@@ -48,7 +37,7 @@
                             licenseNumber += temp[i];
                         }
                     }
-                    else if ((year >= 2018) && (temp.Length == 8))
+                    else if (temp.Length == 8)
                     {
                         for (int i = 0; i < temp.Length; i++)
                         {
@@ -59,18 +48,99 @@
                     }
                     else
                     {
-                        //input defalt value
-                        if (year < 2018)
-                            licenseNumber = "00-000-00";
-                        else
-                            licenseNumber = "000-00-000";
+                    //input defalt value
+                       Console.WriteLine("error input licenseNumber");
                     }
                 }
             }
 
-            public Date DateBegin { get; set; }
+        private Date dateBegin;
 
+        public Date DateBegin
+        {
+            get { return dateBegin; }
+            set
+            {
+
+                if ((dateBegin.day > 31) || (dateBegin.day < 1))
+                {
+                    Console.WriteLine("day must be between 1 to 31");
+                    dateBegin.day = 1; //defalt value
+                }
+                else
+                    dateBegin.day = value.day;
+
+                if ((dateBegin.month > 12) || (dateBegin.month < 1))
+                {
+                    Console.WriteLine("month must be between 1 to 12");
+                    dateBegin.day = 1; //defalt value
+                }
+                else
+                    dateBegin.month = value.month;
+
+                if (dateBegin.year < 1900) //befor 1900 there weren't buses.
+                {
+                    dateBegin.year = 1900;
+                }
+                else
+                    dateBegin.year = value.year;
+            }
         }
+
+        private double mileage;
+
+        public double Mileage
+        {
+            get { return mileage; }
+            set 
+            { 
+                if(mileage < 0)
+                {
+                    Console.WriteLine("mileage can't be negative");
+                    mileage = 0; //defalt value
+                }
+                else
+                    mileage = value;
+            }
+        }
+
+        private double fuel;
+
+        public double Fuel
+        {
+            get { return fuel; }
+            set
+            {
+                if (fuel < 0)
+                {
+                    Console.WriteLine("fuel can't be negative");
+                    fuel = 0; //defalt value
+                }
+                else
+                    fuel = value;
+            }
+        }
+
+        public Bus(string licenseNumber, Date dateBegin, double mileage, double fuel)
+            //constructor
+        {
+            this.licenseNumber = licenseNumber;
+            this.dateBegin = dateBegin;
+            this.mileage = mileage;
+            this.fuel = fuel;
+        }
+
+        public Bus()
+            //defalt constructor
+        {
+            this.licenseNumber = "000-00-000";
+            this.dateBegin.day = 0;
+            this.dateBegin.month = 0;
+            this.dateBegin.year = 0;
+            this.mileage = 0;
+            this.fuel = 0;
+        }
+    }
     }
 
 
