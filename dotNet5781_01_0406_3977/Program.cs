ea249298c6cae6876593af6f2174aec76949a124
אודיה סעדון <osadoun@g.jct.ac.il>
2020-10-28 12:03:49 +0200

    commit1 Odeya

---
+++ dotNet5781_01_0406_3977/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_0406_3977
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
