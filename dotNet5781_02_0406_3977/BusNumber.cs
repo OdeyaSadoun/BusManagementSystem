5105fff1b31a34ff143e6bee71ea80cce7e64383
אודיה סעדון <osadoun@g.jct.ac.il>
2020-11-08 21:35:51 +0200

    begin of the classes

---
+++ dotNet5781_02_0406_3977/BusNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_0406_3977
+{
+    public class BusNumber
+    {
+
+        #region BusLine
+        public int BusLine { get; set; }
+        #endregion
+
+        #region Stations
+        public List<BusLineStation> Stations { get; set; }
+        #endregion
+
+        #region FirstStation
+        public BusStation FirstStation { get; set; }
+        #endregion
+
+        #region LastStation
+        public BusStation LastStation { get; set; }
+        #endregion
+
+        #region Area
+        public string Area { get; set; }
+        #endregion
+
+
+        //Functions:
+
+  //      public BusNumber()
+
+       
+
+      
+    }
+}
---
+++ dotNet5781_02_0406_3977/BusStation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_0406_3977
+{
+    public class BusStation
+    {
+        public static int BusStationKey { get; set; } //לבדוק מה זה מספר סטטי
+
+        #region struct BusStationLocation
+        public struct Location
+        {
+            public double Latitude;
+            public double Longitude;
+        }
+        #endregion
+
+        private Location busStationLocation;
+
+        public Location BusStationLocation
+        {
+            get { return busStationLocation; }
+            set
+            {
+                Random rnd = new Random();
+                busStationLocation.Latitude = rnd.NextDouble() * (33.3 - 31) + 31;
+                busStationLocation.Latitude = rnd.NextDouble() * (35.5 - 34.3) + 34.3;
+            }
+        }
+
+
+        public string BusstationAddress { get; set; }
+
+        //Functions
+
+        public override string ToString()
+        {
+            return string.Format($"Bus Station Code:{0}, {1}°N, {2}°E , {3}", BusStationKey,BusStationLocation.Latitude, BusStationLocation.Longitude, BusstationAddress) ;
+        }
+    }
+}
