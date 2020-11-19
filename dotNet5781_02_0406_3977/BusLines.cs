e29aab593d7df0495d2b78694802fde4090b0a26
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2020-11-19 15:21:09 +0200

    creat new class

---
+++ dotNet5781_02_0406_3977/BusLines.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_0406_3977
+{
+    class BusLines
+    {
+        List<BusLine> buses;
+        public BusLines() { }
+
+    }
+}
