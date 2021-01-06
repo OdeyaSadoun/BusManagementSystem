ea886f9a28f907e9f1fe522041da807ecdd50d83
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-01-06 09:37:39 +0200

    commit

--- PlGui/Login.xaml.cs
+++ PlGui/Login.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace PlGui
 {
@@ -22,6 +25,58 @@ namespace PlGui
         public Login()
         {
             InitializeComponent();
+        }
+        UserWindow user = new UserWindow();
+        AdminWindow admin = new AdminWindow();
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Enter an email.";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Enter a valid email.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string email = textBoxEmail.Text;
+                string password = passwordBox1.Password;
+                SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Registration where Email='" + email + "'  and password='" + password + "'", con);
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count > 0)
+                {
+                    string username = dataSet.Tables[0].Rows[0]["FirstName"].ToString() + " " + dataSet.Tables[0].Rows[0]["LastName"].ToString();
+                    //welcome.TextBlockName.Text = username;//Sending value from one form to another form.  
+                    //welcome.Show();
+                    Close();
+                }
+                else
+                {
+                    errormessage.Text = "Sorry! Please enter existing emailid/password.";
+                }
+                con.Close();
+            }
+        }
+        private void UserRegister_Click(object sender, RoutedEventArgs e)
+        {
+           user.ShowDialog();
+            Close();
+        }
+
+        private void AdinRegister_Click(object sender, RoutedEventArgs e)
+        {
+            admin.ShowDialog();
+            Close();
         }
     }
 }
